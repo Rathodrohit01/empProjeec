8108747e2fda6ad9dc9c67895e8e20a8ede5e8d5
Rathodrohit01 <rathodrohit7218@gmail.com>
2024-10-15 12:06:43 +0530

    commit

---
+++ src/com/controller/EmployeeController.java
@@ -0,0 +1,39 @@
+package com.controller;
+
+import java.util.List;
+
+import com.entity.Employee;
+import com.service.EmployeeService;
+
+public class EmployeeController {
+
+	EmployeeService service = null;
+
+	public List<Employee> getAllEMployee() {
+		service = new EmployeeService();
+		List<Employee> db = service.getAllEmployee();
+		return db;
+	}
+
+	public void insertEmployee(int num) {
+		service = new EmployeeService();
+		service.insertEmployee(num);
+	}
+
+	public boolean update() {
+		service = new EmployeeService();
+		service.update();
+		return true;
+	}
+
+	public boolean deleteEmployee(int id) {
+		service = new EmployeeService();
+		boolean b = service.deleteEmployee(id);
+		return b;
+	}
+
+	public List<Employee> getAllEMPployee() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
